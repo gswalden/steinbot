cef6836a95f05557639204214d7e4e02a4546743
Greg Walden <gswalden@yahoo.com>
2016-07-26 15:14:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -86,10 +86,6 @@ const bot = new Bot({
 }).on('message', message => {
   if (!message.text) return;
   respond(message);
-})
-.on('message', message => {
-  if (!message.text) return;
-  respond(message);
   /*
   const delay = (_.random(99) < 5) ? _.random(ms('2min'), ms('5min')) : 0;
   _.delay(respond, delay, message);
